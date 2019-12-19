349dca74f610f103b48cbef03e225e331e242740
byeon ji young <bjy8668@cmtinfo.co.kr>
2019-12-19 13:05:58 +0900

    spring 초기셜정

---
+++ src/test/java/kr/or/test/MybatisTest.java
@@ -0,0 +1,32 @@
+package kr.or.test;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) //-> 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml"}) //참고할 컨텍스트의 위치 지정
+public class MybatisTest {
+
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testFactory() {
+		System.out.println(sqlSessionFactory);
+	}
+	
+	@Test
+	public void testSession() {
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			System.out.println(session);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
